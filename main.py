e8848144b8188f7a507207257835945bbfb8f1b6
JACOB MARTIN <jcm363@shell>
2019-10-22 16:14:19 -0400

    Added readString function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import re
+
+def readString(toRead):
+    if type(toRead) != str:
+        raise TypeError("Argument is not a string!")
+    ending = re.findall("[A-Za-z]+at ",toRead)
+    filtered = filter(lambda x: len(x) > 4, ending)
+    return filtered
+
+source = open('pg1342.txt')
+pride = source.read()
+readString(pride)
